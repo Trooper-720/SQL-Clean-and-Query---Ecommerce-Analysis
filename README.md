8fa6137f4f5476f2d46b5b5eb2a51b719b6f2cd9
Trooper-720 <102925834+Trooper-720@users.noreply.github.com>
2026-06-17 16:27:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SQL Clean and Query - Ecommerce Analysis
+A project to learn about using real datasets to clean and query data with actionable insights gained from analysis 
---
+++ commands.sql
@@ -0,0 +1,29 @@
+--Local Database used is PostgreSQL
+--All commands are executed in pgAdmin 4 
+
+--Create DB for CSV data
+
+CREATE DATABASE olist_brazilian_ecom_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+
+--Create the Customers Data Table
+-- Table: public.customers_data
+-- DROP TABLE IF EXISTS public.customers_data;
+CREATE TABLE IF NOT EXISTS public.customers_data
+(
+    customer_unique_id uuid NOT NULL,
+    customer_id uuid NOT NULL,
+    customer_zip_code_prefix integer NOT NULL,
+    customer_city character varying(32) COLLATE pg_catalog."default",
+    customer_state character varying(2) COLLATE pg_catalog."default",
+    CONSTRAINT customers_data_pkey PRIMARY KEY (customer_unique_id)
+);
+TABLESPACE pg_default;
+ALTER TABLE IF EXISTS public.customers_data
+    OWNER to postgres;
